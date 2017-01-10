f195c5b6a193ff6f7aeb36a2fbdc731bedee642c
Raphael Yoshiga <ryoshiga@veinteractive.com>
2017-01-10 22:57:24 +0000

    Initial commit

---
+++ GameOfLife.Kata.UnitTests/GameOfLifeShould.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace GameOfLife.Kata.UnitTests
+{
+    [TestFixture]
+    public class GameOfLifeShould
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
